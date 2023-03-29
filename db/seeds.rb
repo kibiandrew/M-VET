54be0e16b2fc62437de0c00d1ea4fd65268e5974
KEVIN-DEVV <kyevenkims@gmail.com>
2023-03-29 09:57:49 +0300

    seeds data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# Seeds for Users
+User.create(name: "John Doe", email: "johndoe@example.com", password: "password")
+User.create(name: "Jane Doe", email: "janedoe@example.com", password: "password")
+
+# Seeds for Pets
+Pet.create(name: "Fluffy", species: "cat", user_id: 1)
+Pet.create(name: "Fido", species: "dog", user_id: 1)
+Pet.create(name: "Buddy", species: "dog", user_id: 2)
+Pet.create(name: "Whiskers", species: "cat", user_id: 2)

c8b00725612bb8c18b07d87ef5b892e7b0485892
KEVIN-DEVV <kyevenkims@gmail.com>
2023-03-29 22:05:10 +0300

    add controllers

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,21 +1,56 @@
 class PetsController < ApplicationController
-    def index
+def index
       @pets = current_user.pets
     end
   
-    def create
-      @pet = current_user.pets.build(pet_params)
-      if @pet.save
-        redirect_to pets_path, notice: "Pet was successfully created."
-      else
-        render "new"
+    def index
+      if params[:user_id]
+        user=User.find(params[:user_id])
+        pet=user.pets
+        render json: pet
       end
+    end
+      def show
+        if params[:user_id]
+          user=User.find(params[:user_id])
+          pet=user.pets
+          pet_ID=pet.find(params[:id])
+          render json: pet_ID
+        end
+      end
+
+    def destroy
+      if params[:user_id]
+        user=User.find(params[:user_id])
+        pet=user.pets
+        pet_ID=pet.find(params[:id])
+        pet_ID.destroy
+      end
+      end
+    def update
+      if params[:user_id]
+        user=User.find(params[:user_id])
+        pet=user.pets
+        pet_ID=pet.find(params[:id])
+        pet_ID.update(pet_params)
+        render json: pet_ID
+      end
+
+    end
+
+    def create
+      if params[:user_id]
+      user=User.find(params[:user_id])
+      pet=user.pets
+      pet.create(pet_params)
+      end
+
     end
   
     private
   
     def pet_params
-      params.require(:pet).permit(:name, :species, :breed, :age)
+      params.permit(:name, :species, :breed, :age)
     end
   end
   

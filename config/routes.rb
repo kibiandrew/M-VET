70bcaadaf4027da035c7b01700f72e9e6a7064f2
KEVIN-DEVV <kyevenkims@gmail.com>
2023-03-29 22:05:42 +0300

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  resources :users, only: [:create] do
-    collection do
-      post 'login'
-    end
+  resources :users do 
+    resources :pets
   end
-
-  resources :pets, only: [:create, :index]
+  resources :pets
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   post 'signup', to: 'users#create'

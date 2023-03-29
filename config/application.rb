ff3279e76f34eb62d1bf582c463d290f17e46952
KEVIN-DEVV <kyevenkims@gmail.com>
2023-03-29 15:01:26 +0300

    altered controllers

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module MVet
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
@@ -30,7 +31,10 @@ module MVet
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
 
+config.action_dispatch.cookies_same_site_protection = :strict
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.

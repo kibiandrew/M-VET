cab461e95a93ab05b41a7f5d1941457bbfc6b87b
KEVIN-DEVV <kyevenkims@gmail.com>
2023-03-29 15:00:54 +0300

    altered controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,33 @@
 class UsersController < ApplicationController
     def create
-      user = User.new(user_params)
-      if user.save
+      user = User.create(user_params)
+      if user.valid? 
         session[:user_id] = user.id
-        redirect_to root_url, notice: "Thank you for signing up!"
-      else
-        render "new"
+        render json: user
       end
+
     end
   
     def login
-      user = User.find_by(email: params[:email])
-      if user && user.authenticate(params[:password])
-        session[:user_id] = user.id
+      @user = User.find_by(email: params[:email])
+  
+      if @user&.authenticate(params[:password])
+        sign_in(@user)
         redirect_to root_url, notice: "Logged in!"
       else
         flash.now.alert = "Email or password is invalid"
-        render "new"
+        render :new
       end
     end
   
     private
   
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.permit(:name, :email, :password, :password_confirmation)
+    end
+  
+    def sign_in(user)
+      session[:user_id] = user.id
     end
   end
   
